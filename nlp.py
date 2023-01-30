bfbb8f2dc26751dfda6630ea344d6b41f628c5ff
Gonçalo Gonçalves <123240910+ggoncalves956@users.noreply.github.com>
2023-01-30 13:09:15 +0100

    Add files via upload

---
+++ nlp.py
@@ -0,0 +1,31 @@
+import spacy
+import re
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+nlp = spacy.load("en_core_web_sm")
+countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the", "Costa Rica", "Côte d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia, Federated States of", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan"]
+
+def get_jurisdiction(contract_text):
+    doc = nlp(contract_text)
+    countries = []
+    for ent in doc.ents:
+        if ent.label_ == "GPE":
+            countries.append(ent.text)
+    if countries:
+        return countries[0]
+    else:
+        return "Could not determine jurisdiction"
+
+@app.route('/')
+def contract_form():
+    return render_template('contract_form.html')
+
+@app.route('/jurisdiction', methods=['POST'])
+def contract_jurisdiction():
+    contract_text = request.form['contract_text']
+    jurisdiction = get_jurisdiction(contract_text)
+    return jurisdiction
+
+if __name__ == '__main__':
+    app.run(debug=True)
